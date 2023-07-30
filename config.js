3fb49654e403b0ece53e79ba7db8006e172ce110
jehan18 <95679613+jehan18@users.noreply.github.com>
2023-07-30 13:34:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,20 +23,20 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 
 
 global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
+global.author = "JEHAN SHAMIKA"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
 global.myweb = "Enter Your Any Website Profile Link Here"
-global.footer = "©\t" + "Enter Your Watermark Here"
+global.footer = "©\t" + "jehan"
 global.prefa = ['-'] // single prefix
 global.themeemoji = "🎐"
 global.autoBio = "$mode : $time ⌚ || $date 🗓️ || $botname" // Leave it as it is if you didn't understood!
 global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
-global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
+global.socialLink = `*⪧⪢ https://wa.me/qr/5XTG7J3MKILYD1 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
-global.ytname = "YT: Enter Your Channel Name Here"
+global.ownername = "JEHAN SHAMIKA"    
+global.ownernumber = ["94782473937"]
+global.ytname = "YT: With js"
 global.continent = "Asia" // your continent name 
 global.region = "Enter Your Country Name Here"
 global.state = "Enter Your State Name Here" // your state name
